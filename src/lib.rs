30cce0a01c0f3d31731c88b318833ec6b0b4fdc1
Io <luke114@outlook.com>
2022-11-05 18:12:58 -0600

    remove simple string

--- src/lib.rs
+++ src/lib.rs
@@ -37,27 +37,27 @@ fn string_ode(cond: &Vec<f32>, step_size: f32, mass: f32, stiffness: f32) -> Vec
 }
 
 
-#[pyfunction]
-fn simple_string(initial_condition: [f32; 6], iterations: usize, step_size: f32) -> Vec<[f32; 6]> {
-    let mut solution: Vec<[f32; 6]> = Vec::new();
-    solution.push(initial_condition);
-    for i in 0..iterations-1 {
-        solution.push(simple_string_ode(solution[i], step_size));
-    }
-    solution
-}
+// #[pyfunction]
+// fn simple_string(initial_condition: [f32; 6], iterations: usize, step_size: f32) -> Vec<[f32; 6]> {
+//     let mut solution: Vec<[f32; 6]> = Vec::new();
+//     solution.push(initial_condition);
+//     for i in 0..iterations-1 {
+//         solution.push(simple_string_ode(solution[i], step_size));
+//     }
+//     solution
+// }
 
 
-fn simple_string_ode(cond: [f32; 6], step_size: f32) -> [f32; 6] {
-    [
-        cond[0] + step_size * cond[3],
-        cond[1] + step_size * cond[4],
-        cond[2] + step_size * cond[5],
-        cond[3] + step_size * STIFFNESS/MASS * (cond[1] - 2.0 * cond[0]),
-        cond[4] + step_size * STIFFNESS/MASS * (cond[0] + cond[2] - 2.0 * cond[1]),
-        cond[5] + step_size * STIFFNESS/MASS * (cond[1] - 2.0 * cond[2]),
-    ]
-}
+// fn simple_string_ode(cond: [f32; 6], step_size: f32) -> [f32; 6] {
+//     [
+//         cond[0] + step_size * cond[3],
+//         cond[1] + step_size * cond[4],
+//         cond[2] + step_size * cond[5],
+//         cond[3] + step_size * STIFFNESS/MASS * (cond[1] - 2.0 * cond[0]),
+//         cond[4] + step_size * STIFFNESS/MASS * (cond[0] + cond[2] - 2.0 * cond[1]),
+//         cond[5] + step_size * STIFFNESS/MASS * (cond[1] - 2.0 * cond[2]),
+//     ]
+// }
 
 
 #[pyfunction]
